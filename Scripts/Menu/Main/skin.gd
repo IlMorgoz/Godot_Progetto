616054e78ed625e2d555e94e7d1828ecc691aeee
IlMorgoz <157831047+IlMorgoz@users.noreply.github.com>
2026-01-29 13:00:41 +0100

    Aggiunta la navicella Aqua ai personaggi giocabili. Animazione scudo presente, ma non funzionale a livello di gameplay

--- Scripts/Menu/Main/skin.gd
+++ Scripts/Menu/Main/skin.gd
@@ -2,6 +2,7 @@ extends Panel
 
 @onready var seleziona1 = $Seleziona
 @onready var seleziona2 = $Seleziona2
+@onready var seleziona3 = $Seleziona3
 @onready var nodo = get_parent()
 
 func _on_seleziona_pressed() -> void:
@@ -10,5 +11,8 @@ func _on_seleziona_pressed() -> void:
 func _on_seleziona_2_pressed() -> void:
 	Global.selected_ship_scene = preload("res://scenes/Spaceships/Players/Flash_Player.tscn")
 
+func _on_seleziona_3_pressed() -> void:
+	Global.selected_ship_scene = preload("res://scenes/Spaceships/Players/Aqua.tscn")
+	
 func _on_back_pressed() -> void:
 	nodo.turn_on(self)
